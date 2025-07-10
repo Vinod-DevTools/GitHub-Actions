209bfb4fdb7c7bd3c8d52ef4c9ee4d2db746601f
Vinod-DevTools <vinodm@devtools.in>
2025-07-10 12:23:25 +0530

    Update __init__.py

--- my-flask-app1/function_app/__init__.py
+++ my-flask-app1/function_app/__init__.py
@@ -1,7 +1,5 @@
-# my-flask-app1/function_app/__init__.py
-
 import azure.functions as func
-from app import app  # your Flask app in app.py
+from app import app  # Import your Flask app
 
 def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
     return func.WsgiMiddleware(app.wsgi_app).handle(req, context)
