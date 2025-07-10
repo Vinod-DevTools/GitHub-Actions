31d95475363576c42d726c4a76c2d946727bd654
Vinod-DevTools <vinodm@devtools.in>
2025-07-10 17:59:36 +0530

    Update __init__.py

--- my-flask-function/function_app/__init__.py
+++ my-flask-function/function_app/__init__.py
@@ -1,11 +1,4 @@
 import azure.functions as func
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    name = req.params.get('name')
-    if not name:
-        return func.HttpResponse(
-            "Please pass a name in the query string.",
-            status_code=400
-        )
-    return func.HttpResponse(f"Hello, {name}!")
-
+    return func.HttpResponse("✅ Hello from Azure Function App!", status_code=200)
