74b0d46197e0c429c75077b97a0e76d2a072fdec
Vinod-DevTools <vinodm@devtools.in>
2025-07-10 12:23:00 +0530

    Update app.py

--- my-flask-app1/app.py
+++ my-flask-app1/app.py
@@ -1,8 +1,7 @@
-# my-flask-app1/app.py
-
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello from Azure Function Flask app!'
+@app.route("/")
+def hello():
+    return "Hello from Azure Function + Flask!"
