f80585f3bd7f3f7e7a82c24e703583a7d51596ef
Vinod-M-git <vinodm@devtools.in>
2025-07-10 17:53:13 +0530

    update

--- my-flask-function/function_app/__init__.py
+++ my-flask-function/function_app/__init__.py
@@ -1,6 +1,11 @@
 import azure.functions as func
-from app import app
-from azure.functions.wsgi import WsgiMiddleware
 
-main = WsgiMiddleware(app).main
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    name = req.params.get('name')
+    if not name:
+        return func.HttpResponse(
+            "Please pass a name in the query string.",
+            status_code=400
+        )
+    return func.HttpResponse(f"Hello, {name}!")
 
