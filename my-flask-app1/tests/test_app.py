64245d6fa1344b92d8721a6dd3734192c5a08760
Vinod-DevTools <vinodm@devtools.in>
2025-07-10 12:14:45 +0530

    Update test_app.py

--- my-flask-app1/tests/test_app.py
+++ my-flask-app1/tests/test_app.py
@@ -1,4 +1,8 @@
-import sys
-import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+# my-flask-app1/tests/test_app.py
+
 from app import app
+
+def test_home_route():
+    with app.test_client() as client:
+        response = client.get('/')
+        assert response.status_code == 200
